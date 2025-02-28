e1b53d4f3c05b7aac97dd88eb5340bf605fcad04
ShivmMakone <shivammakone@gmail.com>
2025-02-28 05:44:16 +0530

    Add files via upload
    This program takes an integer input from the user and determines whether the number is even or odd using an if-else statement.

---
+++ Task1.py
@@ -0,0 +1,8 @@
+# Taking user input
+num = int(input("Enter a number: "))
+
+# Checking even or odd and displaying the result
+if num % 2 == 0:
+    print(f"\n{num} is an even number.")
+else:
+    print(f"\n{num} is an odd number.")
